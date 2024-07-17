3104391b8b4725d87a9b58c1d6eae60b81593cb2
angel luna <angelstivengarcialuna@gmail.com>
2024-07-17 12:30:43 -0500

    Arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { Boton, Video, BotonUbicacion, Iconos, Sonrisa } from "./components/index";
+import {
+  Boton,
+  Video,
+  BotonUbicacion,
+  Iconos,
+  Sonrisa,
+} from "./components/index";
 import logo from "./assets/logo.png";
 import iconWhatsapp from "./assets/whatsapp.png";
 import maps from "./assets/mapas-y-banderas.png";
@@ -8,133 +14,50 @@ import dinete3 from "./assets/dientes3.svg";
 import dinete4 from "./assets/dientes4.svg";
 import dinete5 from "./assets/dientes5.svg";
 import dinete6 from "./assets/dientes6.svg";
-import dientecito from "./assets/dientecito.svg"
+import dientecito from "./assets/dientecito.svg";
 const App = () => {
   return (
     <>
-      {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-      <div style={{
-        marginRight : "50px",
-      }}>
-      {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-      <img
-        src={logo}
+      <div
         style={{
           width: "700px",
-        }}
-      />
-        {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-        <img
-          src={dinetes}
-          style={{
-            width: "350px",
-            // ajusta la posición vertical
-            margin: "auto",
-            position: "absolute",
-            top: "500px",
-            left: "500px",
-            zIndex: -1, // establece el índice Z,
-            transform: "rotate(175deg)",
-          }}
-        />
-        {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-        <img
-          src={dinete2}
-          style={{
-            width: "470px",
-            // ajusta la posición vertical
-            margin: "auto",
-            position: "absolute",
-            top: "440px",
-            left: "450px",
-            zIndex: -2, // establece el índice Z,
-            transform: "rotate(175deg)",
-          }}
-        />
-        {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-        <img
-          src={dinete3}
-          style={{
-            width: "570px",
-            // ajusta la posición vertical
-            margin: "auto",
-            position: "absolute",
-            top: "390px",
-            left: "400px",
-            zIndex: -3, // establece el índice Z,
-            transform: "rotate(175deg)",
-          }}
-        />
-        {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-        <img
-          src={dinete4}
-          style={{
-            width: "670px",
-            // ajusta la posición vertical
-            margin: "auto",
-            position: "absolute",
-            top: "340px",
-            left: "350px",
-            zIndex: -4, // establece el índice Z,
-            transform: "rotate(175deg)",
-          }}
-        />
-        {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-        <img
-          src={dinete5}
-          style={{
-            width: "800px",
-            // ajusta la posición vertical
-            margin: "auto",
-            position: "absolute",
-            top: "290px",
-            left: "300px",
-            zIndex: -5, // establece el índice Z,
-            transform: "rotate(175deg)",
-          }}
-        />
-{/* biome-ignore lint/a11y/useAltText: <explanation> */}
-<img
-  src={dinete6}
-  style={{
-    width: "1200px", // Aumenta el ancho de la imagen
-    margin: "auto",
-    position: "absolute",
-    top: "250px",
-    left: "250px",
-    zIndex: -10,
-    transform: "rotate(175deg)",
-  }}
-/>
-{/* <img
-  src={dientecito}
-  style={{
-    width: "1000px", // Aumenta el ancho de la imagen
-    margin: "auto",
-    position: "absolute",
-    top: "250px",
-    left: "200px",
-    zIndex: -11,
-    transform: "rotate(175deg)",
-  }}
-/> */}
+          margin: "0 auto",
 
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* biome-ignore lint/a11y/useAltText: <explanation> */}
+        <img
+          src={logo}
+          style={{
+           
+            width: "500px",
+            height: "180px",
+            zIndex : 99999
+          }}
+        />
       </div>
 
- 
-      <center>
-        <br />
-        <br />
+      <div className="container">
+      {/* biome-ignore lint/a11y/noRedundantAlt: <explanation> */}
+      <img src={dinetes} className="image image-1" alt="Image 1" />
+      {/* biome-ignore lint/a11y/noRedundantAlt: <explanation> */}
+      <img src={dinete2} className="image image-2" alt="Image 2" />
+      {/* biome-ignore lint/a11y/noRedundantAlt: <explanation> */}
+      <img src={dinete3} className="image image-3" alt="Image 3" />
+      {/* biome-ignore lint/a11y/noRedundantAlt: <explanation> */}
+      <img src={dinete4} className="image image-4" alt="Image 4" />
+      {/* biome-ignore lint/a11y/noRedundantAlt: <explanation> */}
+      <img src={dinete5} className="image image-5" alt="Image 5" />
+      {/* biome-ignore lint/a11y/noRedundantAlt: <explanation> */}
+      <img src={dinete6} className="image image-6" alt="Image 6" />
+    </div>
 
-        <h1 className="titulo">CUCUTÁ</h1>
-      </center>
-
-      {/* biome-ignore lint/a11y/useAltText: <explanation> */}
+      <h1 className="titulo">CUCUTÁ</h1>
 
       <Video />
-      <br />
-      <br />
-      <br />
 
       <Boton
         icono={iconWhatsapp}
@@ -148,15 +71,8 @@ const App = () => {
         link="https://api.whatsapp.com/send?phone=573224130747&text=Hola, quisiera saber información sobre."
       />
       <center>
-        <br />
-        <br />
-
         <h1 className="titulo">MEDELLÍN</h1>
         <Video />
-        <br />
-        <br />
-        <br />
-
         <Boton
           icono={iconWhatsapp}
           titulo="AGENDE SU CITA"
