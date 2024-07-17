38bd5dbf2b92ae1c72091cdee8427bc14b0bb809
angel luna <angelstivengarcialuna@gmail.com>
2024-07-17 17:05:40 -0500

    Arreglando cosas de los link

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
       <BotonUbicacion
         icono={maps}
         titulo="UBICACION"
-        link="https://api.whatsapp.com/send?phone=573224130747&text=Hola, quisiera saber información sobre."
+        link="https://www.google.com/maps/place/Cl+2+%239e80,+Quinta+Oriental,+Cúcuta,+Norte+de+Santander/@7.8977635,-72.490802,3a,75y,5.45h,68.88t/data=!3m6!1e1!3m4!1s6XSXqplLG0EMQyrDVbYHQQ!2e0!7i16384!8i8192!4m7!3m6!1s0x8e664512041c2cd9:0x1c5f10ebd8b71a2!8m2!3d7.8978775!4d-72.4913395!10e5!16s%2Fg%2F11jdmhbl3g"
       />
       <h1 className="titulo">MEDELLÍN</h1>
       <Video />
@@ -71,12 +71,12 @@ const App = () => {
       <BotonMedellin
         icono={iconWhatsapp}
         titulo="AGENDE SU CITA"
-        link="https://api.whatsapp.com/send?phone=573224130747&text=Hola, quisiera saber información sobre."
+        link="https://api.whatsapp.com/send?phone=573016184618&text=Hola, quisiera saber información sobre."
       />
       <BotonUbicacion
         icono={maps}
         titulo="UBICACION"
-        link="https://api.whatsapp.com/send?phone=573224130747&text=Hola, quisiera saber información sobre."
+        link="https://www.google.com/maps/place/Centro+Empresarial+Dann+-+Torre+Dann/@6.2071307,-75.5715028,17.5z/data=!4m6!3m5!1s0x8e468281d507240f:0x1b5906f8523b9305!8m2!3d6.2072299!4d-75.5710209!16s%2Fg%2F1hf2lyz6l?hl=es"
       />
 
       <Iconos />
--- src/components/iconos.jsx
+++ src/components/iconos.jsx
@@ -23,9 +23,9 @@ export const Iconos = () => {
             position : "relative",
             zIndex: 900
           }}
-            href="https://www.bing.com/search?pglt=43&q=quizzes&cvid=687afced86464b9387e3e76ddb7d96f7&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARAAGEAyBggCEAAYQDIGCAMQABhAMgYIBBAAGEAyBggFEAAYQDIGCAYQABhAMgYIBxAAGEAyBggIEEUYPdIBCDQ3MDRqMGoxqAIAsAIA&FORM=ANNTA1&PC=HCTS&ntref=1"
-            aria-label="Discord"
-            data-social="discord"
+            href="https://www.facebook.com/Oraltechpage/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0https://www.facebook.com/tu_pagina_de_facebook"
+            aria-label="Facebook"
+            data-social="Facebook"
           >
             <img src={diente} style={{ width: "120px",cursor: 'pointer' }} alt="Diente" />
             <img src={facebook} alt="Facebook"  style={{ width: "45px",       position : "relative", top : "-110px"}}/>
@@ -35,13 +35,13 @@ export const Iconos = () => {
           <a
            style={{
         
-          
+          backgroundColor : "white",
             position : "relative",
-            zIndex: 900
+            zIndex: 9
           }}
-            href="https://store.steampowered.com/"
-            aria-label="Steam"
-            data-social="steam"
+            href="https://www.instagram.com/oraltech_clinicadental/?igshid=MWI4MTIyMDE%3D"
+            aria-label="Instragram"
+            data-social="Instragram"
           >
             <img src={diente} style={{ width: "120px", cursor: 'pointer', }} alt="Diente" />
             <img src={instragram} alt="Instragram"  style={{ width: "45px",       position : "relative", top : "-109px",left : "2px"}}/>
@@ -52,11 +52,11 @@ export const Iconos = () => {
           <a
       style={{
         position : "relative",
-        zIndex: 900
+        zIndex: 90
       }}
-            href="https://www.instagram.com/"
-            aria-label="Instagram"
-            data-social="instagram"
+            href="https://www.tiktok.com/@oraltechodontologia"
+            aria-label="tik-tok"
+            data-social="tik-tok"
           >
             <img src={diente} style={{ width: "120px" ,cursor: 'pointer',}} alt="Diente" />
             <img src={tiktok} alt="tik-tok"  style={{ width: "45px",       position : "relative", top : "-110px" }}/>
