9afdeaee530c215dad99b0233abc297349585926
angel luna <angelstivengarcialuna@gmail.com>
2025-05-13 11:48:37 -0500

    Agregando el mejorando el codigo en los metadatos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,9 @@ import "../assets/globals.css";
 export const metadata: Metadata = {
   // Basic Metadata
   title: "OralTech",
-  description: "Somos un consultorio odontológico en el que nos dedicamos a crear sonrisas radiantes que irradian confianza y belleza, buscamos hacer felices a nuestros pacientes ayudándolos a conseguir su sonrisa soñada. Contamos con los profesionales ideales para ayudar a nuestros pacientes a mantener su salud y estética bucal.",
-  
+  description:
+    "Somos un consultorio odontológico en el que nos dedicamos a crear sonrisas radiantes que irradian confianza y belleza, buscamos hacer felices a nuestros pacientes ayudándolos a conseguir su sonrisa soñada. Contamos con los profesionales ideales para ayudar a nuestros pacientes a mantener su salud y estética bucal.",
+
   // Icons
   icons: {
     icon: "/favicon.ico",
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
       url: "/favicon.ico",
     },
   },
-  
+
   // App Information
   applicationName: "OralTech",
   appleWebApp: {
@@ -24,11 +25,12 @@ export const metadata: Metadata = {
     title: "OralTech",
     statusBarStyle: "default",
   },
-  
+
   // Open Graph Protocol (for social media sharing)
   openGraph: {
     title: "OralTech",
-    description: "Somos un consultorio odontológico en el que nos dedicamos a crear sonrisas radiantes que irradian confianza y belleza, buscamos hacer felices a nuestros pacientes ayudándolos a conseguir su sonrisa soñada. Contamos con los profesionales ideales para ayudar a nuestros pacientes a mantener su salud y estética bucal.",
+    description:
+      "Somos un consultorio odontológico en el que nos dedicamos a crear sonrisas radiantes que irradian confianza y belleza, buscamos hacer felices a nuestros pacientes ayudándolos a conseguir su sonrisa soñada. Contamos con los profesionales ideales para ayudar a nuestros pacientes a mantener su salud y estética bucal.",
     url: "https://www.oraltechodontologia.com/",
     siteName: "OralTech",
     locale: "es_CO",
@@ -43,25 +45,25 @@ export const metadata: Metadata = {
       },
     ],
   },
-  
+
   // Twitter Card
   twitter: {
     card: "summary_large_image",
     title: "OralTech",
-    description: "Somos un consultorio odontológico en el que nos dedicamos a crear sonrisas radiantes que irradian confianza y belleza, buscamos hacer felices a nuestros pacientes ayudándolos a conseguir su sonrisa soñada. Contamos con los profesionales ideales para ayudar a nuestros pacientes a mantener su salud y estética bucal.",
+    description:
+      "Somos un consultorio odontológico en el que nos dedicamos a crear sonrisas radiantes que irradian confianza y belleza, buscamos hacer felices a nuestros pacientes ayudándolos a conseguir su sonrisa soñada. Contamos con los profesionales ideales para ayudar a nuestros pacientes a mantener su salud y estética bucal.",
     images: ["../assets/webp/oraltech_logo_white.webp"],
     site: "@OralTech",
-    
+
     creator: "@AngelLuna",
   },
-  
+
   // Robots and Indexing
   robots: {
     index: true,
     follow: true,
 
     googleBot: {
-
       index: true,
       follow: true,
       "max-video-preview": -1,
@@ -69,7 +71,7 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  
+
   // Alternate Languages
   alternates: {
     canonical: "https://www.oraltechodontologia.com",
@@ -78,8 +80,8 @@ export const metadata: Metadata = {
       "en-US": "https://www.oraltechodontologia.com/en",
     },
   },
-  
-   // Verification for Search Console and Other Services
+
+  // Verification for Search Console and Other Services
   verification: {
     google: "google-site-verification-code",
     yandex: "yandex-verification-code",
@@ -87,40 +89,40 @@ export const metadata: Metadata = {
       "facebook-domain-verification": "facebook-domain-verification-code",
       "norton-safeweb-site-verification": "norton-verification-code",
       "baidu-site-verification": "baidu-verification-code",
-      "pinterest-site-verification": "pinterest-verification-code"
-    }
+      "pinterest-site-verification": "pinterest-verification-code",
+    },
   },
   // Additional Metadata
   category: "Dental",
   creator: "Angel Luna",
   publisher: "OralTech",
   keywords: [
-    "tecnología dental", 
-    "equipos dentales", 
-    "servicio técnico dental", 
-    "OralTech", 
-    "clínica dental", 
-    "odontología", 
-    "Cúcuta", 
-    "Medellín", 
-    "Colombia", 
-    "equipos odontológicos", 
+    "tecnología dental",
+    "equipos dentales",
+    "servicio técnico dental",
+    "OralTech",
+    "clínica dental",
+    "odontología",
+    "Cúcuta",
+    "Medellín",
+    "Colombia",
+    "equipos odontológicos",
     "mantenimiento dental",
     "innovación dental",
     "software dental",
-    "radiología dental digital"
+    "radiología dental digital",
   ],
-  
+
   // Manifest
   manifest: "/site.webmanifest",
-  
+
   // Viewport Settings
   viewport: {
     width: "device-width",
     initialScale: 1,
     maximumScale: 5,
   },
-  
+
   // Mobile App Install Banners
   formatDetection: {
     telephone: true,
@@ -129,16 +131,16 @@ export const metadata: Metadata = {
     email: true,
     url: true,
   },
-  
+
   // Referrer Policy
   referrer: "origin-when-cross-origin",
-  
+
   // Theme Color
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#28295F" },
     { media: "(prefers-color-scheme: dark)", color: "#28295F" },
   ],
-  
+
   // Content Type
   other: {
     "Content-Type": "text/html; charset=utf-8",
