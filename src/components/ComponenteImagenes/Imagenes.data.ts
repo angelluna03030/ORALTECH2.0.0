2166391c512343faff2682c3548e060ce148409a
angel luna <angelstivengarcialuna@gmail.com>
2025-06-14 03:13:20 -0500

    Agregandole data e imagnes a el compoenten imagenes

--- src/components/ComponenteImagenes/Imagenes.data.ts
+++ src/components/ComponenteImagenes/Imagenes.data.ts
@@ -14,10 +14,11 @@ export const servicios: ComponenteImagenesProps[] = [
     src: DiseñoSonrisa.src,
     alt: "Diseño de Sonrisa",
     titulo: "Diseño de Sonrisa",
-    descripcion: "El diseño de sonrisa es un procedimiento odontológico estético que busca mejorar la apariencia de la sonrisa de una persona a través de la corrección de la forma, tamaño, color y alineación de los dientes, buscando la armonía con el rostro del paciente. Este proceso involucra varias especialidades odontológicas y técnicas personalizadas para lograr una sonrisa más estética, funcional y natural ",
+    descripcion:
+      "El diseño de sonrisa es un procedimiento odontológico estético que busca mejorar la apariencia de la sonrisa de una persona a través de la corrección de la forma, tamaño, color y alineación de los dientes, buscando la armonía con el rostro del paciente. Este proceso involucra varias especialidades odontológicas y técnicas personalizadas para lograr una sonrisa más estética, funcional y natural ",
     imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886793/WhatsApp_Image_2025-06-14_at_2.20.06_AM_mtucbq.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886792/WhatsApp_Image_2025-06-14_at_2.20.07_AM_1_hwnxbr.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
@@ -28,13 +29,14 @@ export const servicios: ComponenteImagenesProps[] = [
     src: Ortodoncia.src,
     alt: "Tratamiento de ortodoncia con alineadores transparentes",
     titulo: "Ortodoncia",
-    descripcion: "  La ortodoncia es una especialidad de la odontología que se enfoca en corregir problemas de alineación dental y de mordida. Su objetivo principal es mejorar la función masticatoria, la salud bucal y la estética facial, logrando una sonrisa más armoniosa y saludable.",
+    descripcion:
+      "  La ortodoncia es una especialidad de la odontología que se enfoca en corregir problemas de alineación dental y de mordida. Su objetivo principal es mejorar la función masticatoria, la salud bucal y la estética facial, logrando una sonrisa más armoniosa y saludable.",
     imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886792/WhatsApp_Image_2025-06-14_at_2.24.41_AM_3_scolr0.jpg",
+
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749887185/WhatsApp_Image_2025-06-14_at_2.20.09_AM_1_k7g2gq.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749887185/WhatsApp_Image_2025-06-14_at_2.20.09_AM_klkiro.jpg",
     ],
   },
   {
@@ -42,13 +44,13 @@ export const servicios: ComponenteImagenesProps[] = [
     src: MicroDiseño.src,
     alt: "Microdiseño de Sonrisa con técnicas avanzadas",
     titulo: "Microdiseño de Sonrisa",
-    descripcion: " El Microdiseño de sonrisa es un tratamiento de odontología estética conservadora, se realiza de forma rápida, sencilla y segura. Beneficios Mejora tu sonrisa de forma rápida y sencilla Procedimiento rápido y sin traumas No causa molestias ni dolor Sin desgaste dental Resultados muy naturales, porque el diseño se basa en la  forma del del diente del paciente No requiere anestesia local porque sólo actúa en la superficie del diente ",
+    descripcion:
+      " El Microdiseño de sonrisa es un tratamiento de odontología estética conservadora, se realiza de forma rápida, sencilla y segura. Beneficios Mejora tu sonrisa de forma rápida y sencilla Procedimiento rápido y sin traumas No causa molestias ni dolor Sin desgaste dental Resultados muy naturales, porque el diseño se basa en la  forma del del diente del paciente No requiere anestesia local porque sólo actúa en la superficie del diente ",
     imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886793/WhatsApp_Image_2025-06-14_at_2.19.52_AM_k72f0a.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886793/WhatsApp_Image_2025-06-14_at_2.19.50_AM_4_ztdhpz.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886793/WhatsApp_Image_2025-06-14_at_2.20.03_AM_cdo5vz.jpg",
     ],
   },
   {
@@ -58,11 +60,11 @@ export const servicios: ComponenteImagenesProps[] = [
     titulo: "Limpieza Dental",
     descripcion: "Higiene profesional para una boca sana",
     imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886792/WhatsApp_Image_2025-06-14_at_2.20.08_AM_by4dwn.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886792/WhatsApp_Image_2025-06-14_at_2.20.02_AM_qyznsd.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749886792/WhatsApp_Image_2025-06-14_at_2.20.07_AM_1_hwnxbr.jpg",
     ],
   },
   {
@@ -70,74 +72,67 @@ export const servicios: ComponenteImagenesProps[] = [
     src: Blanquimineto.src,
     alt: "Procedimiento de blanqueamiento dental profesional",
     titulo: "Blanqueamiento",
-    descripcion: " El blanqueamiento dental es un procedimiento de odontología estética que busca un aclaramiento mediante la eliminación de manchas dentales Beneficios: Elimina manchas dentales Una sonrisa más estética Una buena dosis de confianza Preserva la salud oral Es indoloro",
+    descripcion:
+      " El blanqueamiento dental es un procedimiento de odontología estética que busca un aclaramiento mediante la eliminación de manchas dentales Beneficios: Elimina manchas dentales Una sonrisa más estética Una buena dosis de confianza Preserva la salud oral Es indoloro",
     imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749887456/WhatsApp_Image_2025-06-14_at_2.19.41_AM_jh537w.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749887457/WhatsApp_Image_2025-06-14_at_2.24.41_AM_2_xpzgsf.jpg",
+
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749887456/WhatsApp_Image_2025-06-14_at_2.19.50_AM_1_sloqzk.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749887457/WhatsApp_Image_2025-06-14_at_2.24.41_AM_2_xpzgsf.jpg",
+    ],
+  },
+];
+
+export const services: ComponenteImagenesProps[] = [
+  {
+    id: 6,
+    titulo: "Implante Dental",
+    src: ImplanteDentales.src,
+    descripcion: "Dientes más blancos y brillantes",
+    alt: "Implante Dental",
+    imagenes: [
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.51_AM_2_wr58sg.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888071/WhatsApp_Image_2025-06-14_at_2.59.51_AM_1_rgy5hb.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.52_AM_2_sssqsn.jpg",
+    ],
+  },
+  {
+    id: 7,
+    titulo: "Endodoncia",
+    src: Endodoncia.src,
+    descripcion: "Dientes más blancos y brillantes",
+    alt: "Implante Dental",
+    imagenes: [
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
       "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
     ],
   },
+  {
+    id: 9,
+    titulo: "Incrustación Dental",
+    src: IncrustaciónDental.src,
+    descripcion: "Dientes más blancos y brillantes",
+    alt: "Implante Dental",
+    imagenes: [
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.52_AM_2_sssqsn.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.51_AM_2_wr58sg.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.52_AM_tkqfif.jpg",
+    ],
+  },
+  {
+    id: 10,
+    titulo: "Calza Dental",
+    src: CalzaDental.src,
+    descripcion:
+      "Tienes como objetivo primordial prevenir la aparición de la enfermedad periodontal en los adultos. Para conseguirlo, hay que eliminar la placa dental que se acumula alrededor de los dientes Beneficios: Previene las caries Evita el desarrollo de la enfermedad periodontal Fomenta hábitos de higiene oral adecuados Disminuye el riesgo de enfermedades graves Elimina las manchas dentales Controla el mal aliento Reduce la inversión en tratamientos dentales",
+    alt: "Implante Dental",
+    imagenes: [
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.52_AM_2_sssqsn.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.51_AM_2_wr58sg.jpg",
+      "https://res.cloudinary.com/do8uezira/image/upload/v1749888067/WhatsApp_Image_2025-06-14_at_2.59.52_AM_tkqfif.jpg",
+    ],
+  },
 ];
-
-
-
-export const services : ComponenteImagenesProps[] = [
-    {
-      id: 6,
-      titulo: "Implante Dental",
-      src: ImplanteDentales.src,
-      descripcion: "Dientes más blancos y brillantes",
-      alt :"Implante Dental",
-    imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
-    ],
-    },
-    {
-       id: 7,
-      titulo: "Endodoncia",
-      src: Endodoncia.src,
-          descripcion: "Dientes más blancos y brillantes",
-          alt :"Implante Dental",
-    imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
-    ],
-    },
-    {
-       id: 9,
-      titulo: "Incrustación Dental",
-      src: IncrustaciónDental.src,
-       descripcion: "Dientes más blancos y brillantes",
-       alt :"Implante Dental",
-    imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
-    ],
-    },
-    {
-       id: 10,
-      titulo: "Calza Dental",
-      src: CalzaDental.src,
-       descripcion: "Tienes como objetivo primordial prevenir la aparición de la enfermedad periodontal en los adultos. Para conseguirlo, hay que eliminar la placa dental que se acumula alrededor de los dientes Beneficios: Previene las caries Evita el desarrollo de la enfermedad periodontal Fomenta hábitos de higiene oral adecuados Disminuye el riesgo de enfermedades graves Elimina las manchas dentales Controla el mal aliento Reduce la inversión en tratamientos dentales",
-       alt :"Implante Dental",
-    imagenes: [
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/kwzebqktdkqntwsx3ueq.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/p528j3ntzuorxlcfkmqr.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/o3oabg2uoghqsimnqvct.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/qhcxyhanwdiyck4wrde7.jpg",
-      "https://res.cloudinary.com/doza0twgj/image/upload/v1749782069/uploads/fcmeuheclyb0yxfdixqs.jpg",
-    ],
-    },
-  ];
