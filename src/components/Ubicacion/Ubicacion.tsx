f78f54e5521b6614641caa47a9b5df6c5a8bb8e3
angel luna <angelstivengarcialuna@gmail.com>
2025-06-14 01:30:37 -0500

    Colocando datos reales en testimonials

--- src/components/Ubicacion/Ubicacion.tsx
+++ src/components/Ubicacion/Ubicacion.tsx
@@ -3,15 +3,16 @@ import { Ubicaciones } from "./index";
 import Link from "next/link";
 import { Star } from "lucide-react";
 import ImagenesUbicacion from "@/assets/webp/Recurso 3@4x-8.webp";
+
 export const Ubicacion = () => {
   return (
     <>
-      <div className="text-center mb-28">
+      <div className="text-center mb-44">
         <div className="inline-flex items-center space-x-2 my-5 bg-blue-50 text-[#28295F] px-3 md:px-4 py-2 rounded-full text-xs md:text-sm font-medium">
           <Star className="w-4 h-4 fill-current text-[#28295F] " />
           Ubicaciones Premium
         </div>
-        <div className="flex justify-center w-full">
+        <div className="flex justify-center w-full my-10">
           <Image
             src={ImagenesUbicacion}
             alt="Imagen de Ubicación"
@@ -19,13 +20,13 @@ export const Ubicacion = () => {
             priority
           />
         </div>
-        <p className="text-slate-600 w-full mx-auto text-lg leading-relaxed mb-3 pt-6">
+        <p className="text-slate-600 w-full mx-auto text-lg leading-tight pb-10">
           Dos espacios diseñados con la más alta tecnología y confort para
           brindarte una experiencia odontológica excepcional en cada visita.
         </p>
       </div>
       <section
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  my-10 px-4 max-w-7xl mx-auto"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10 px-4 max-w-7xl mx-auto"
         style={{
           justifyContent: "space-evenly",
           flexDirection: "row",
@@ -49,7 +50,7 @@ export const Ubicacion = () => {
                   title={servicio.alt}
                   width={500}
                   height={500}
-                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                  className="w-full h-full mr- object-cover transition-transform duration-500 group-hover:scale-110"
                 />
               </Link>
               {/* Gradiente superpuesto para mejorar la legibilidad del texto */}
